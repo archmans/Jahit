5b7323230826a3159fc0e59ca65c60f5ec5aec84
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2025-05-05 11:39:17 +0700

    Initial Commit

---
+++ Jahit/JahitApp.swift
@@ -0,0 +1,32 @@
+//
+//  JahitApp.swift
+//  Jahit
+//
+//  Created by Muhamad Salman Hakim Alfarisi on 05/05/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct JahitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ JahitTests/JahitTests.swift
@@ -0,0 +1,17 @@
+//
+//  JahitTests.swift
+//  JahitTests
+//
+//  Created by Muhamad Salman Hakim Alfarisi on 05/05/25.
+//
+
+import Testing
+@testable import Jahit
+
+struct JahitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
